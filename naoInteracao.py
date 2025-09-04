55b7b9494f46e58aa9bc6d61b01aa162db6cd568
DiogoCostaPrauchner <FleureFloral@protonmail.com>
2025-09-04 16:52:33 -0300

    Adicionado mais comentários explicativos

--- naoInteracao.py
+++ naoInteracao.py
@@ -15,9 +15,13 @@ PORT = 9559             #Porta utilizada pelos dois robôs
 
 #Mensagens utilizadas pelos robôs nas conversas
 mensagemsRA = ["Eu sou o Robô 1!", "Como você está hoje, Robô 2?", "Eu também! Ótimo dia hoje, não é mesmo?",
-               "Sejam Bem-Vindos..."]
+               "Sejam Bem-Vindos...", "frase 11"]
 mensagemsRB = ["E eu sou o Robô 2!", "Eu estou ótimo, e você, Robô 1?", "Sim! O sol está lindo!",
-              "Ao Espaço Mais Inovação..."]
+              "Ao Espaço Mais Inovação...", "frase 12"]
+
+#Apenas números para testar mais rapidamente a sequencia da conversa
+#mensagemsRA = ["1", "3", "5", "7", "11"]
+#mensagemsRB = ["2", "4", "6", "8", "12"]
 
 #Inicialização dos contadores utilizados para a lógica de conversa
 cont1 = 0
@@ -41,7 +45,7 @@ mem_r2 = ALProxy("ALMemory", RB_IP, PORT)
 
 #Inicialização para garantir que o código funciona
 mem_r1.insertData("Robo1Fala", "Eu sou o robô 1!")
-mem_r2.insertData("Robo2Fala", "Eu sou o robô 2!")
+mem_r2.insertData("Robo2Fala", "E eu sou o robô 2!")
 
 def main():
     contEnd = 0
@@ -56,7 +60,7 @@ def main():
                 cont1 = (cont1 + 1) % len(mensagemsRA)
                 mem_r2.raiseEvent("Robo2Fala", mensagemsRB[cont2])
                 contEnd += 1
-                print(str(contEnd) + "Valor de contEnd")
+                print("Valor de contEnd: " + str(contEnd))
 
             msg2 = mem_r2.getData("Robo2Fala")
             if msg2 is not None:
@@ -67,10 +71,18 @@ def main():
                 contEnd += 1
                 print("Valor de contEnd: " + str(contEnd))
 
-            if contEnd >= 8:
+            #A partir dessa parte usar apenas contEnd para definir quando os robôs vão falar juntos
+            #ou quando que vai terminar a conversa
+            #Frases 9, 10
+            if contEnd == 8:
                 ttsA_r1.post.say("Da Unijuí!", config)
+                #Usar time.sleep() com um valor baixo pois post.say não está funcionando corretamente
                 time.sleep(0.05)
                 tts_r2.post.say("Da Unijuí!")
+                contEnd += 1
+                print("Valor de contEnd: " + str(contEnd))
+
+            if contEnd == 11:
                 print("Conversa encerrada!")
                 break
 
